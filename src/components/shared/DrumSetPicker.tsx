acb53ba699808df2195b7c8dc47b0fac422f1cc8
PavelS-BM <pavel.stastny@brandmaster.com>
2021-12-27 19:23:12 +0100

    Add synthwawe pack

--- src/components/shared/DrumSetPicker.tsx
+++ src/components/shared/DrumSetPicker.tsx
@@ -13,13 +13,14 @@ export default function DrumSetPicker({ wrapperClasses, onChange }: IDrumSetPick
         <select className="select select-bordered w-full" onChange={(e) => onChange(e.target.value)}>
           <option value="v0">Base</option>
           <option value="v1">Jungle</option>
+          <option value="v8">Synthwawe</option>
           <option value="v2">Base alt 1</option>
           <option value="v3">Base alt 2</option>
           <option value="v4">Base alt 3</option>
           <option value="v5">Jungle alt 1</option>
           <option value="v6">Jungle alt 2</option>
           <option value="v7">Jungle alt 3</option>
-          <option value="v7">Synthwawe</option>
+          <option value="v9">Synthwawe alt 1</option>
         </select>
       </div>
     </div>
